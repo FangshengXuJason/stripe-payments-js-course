8dfe4df248b50416a9900edd0f4c512378c2deda
FangshengXu <91645998+FangshengXuJason@users.noreply.github.com>
2022-07-25 12:36:21 -0700

    changed Awesome Membership's plan id. The current version of API looks pretty different now

--- react-app/src/Subscriptions.js
+++ react-app/src/Subscriptions.js
@@ -1,80 +1,79 @@
-import React, { useState, useEffect, Suspense } from 'react';
-import { fetchFromAPI } from './helpers';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-import { useUser, AuthCheck } from 'reactfire';
+import React, { useState, useEffect, Suspense } from 'react'
+import { fetchFromAPI } from './helpers'
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js'
+import { useUser, AuthCheck } from 'reactfire'
 
-import { db } from './firebase';
-import { SignIn, SignOut } from './Customers';
+import { db } from './firebase'
+import { SignIn, SignOut } from './Customers'
 
 function UserData(props) {
-
-  const [data, setData] = useState({});
+  const [data, setData] = useState({})
 
   // Subscribe to the user's data in Firestore
-  useEffect(
-    () => {
-      const unsubscribe = db.collection('users').doc(props.user.uid).onSnapshot(doc => setData(doc.data()) )
-      return () => unsubscribe()
-    },
-    [props.user]
-  )
+  useEffect(() => {
+    const unsubscribe = db
+      .collection('users')
+      .doc(props.user.uid)
+      .onSnapshot((doc) => setData(doc.data()))
+    return () => unsubscribe()
+  }, [props.user])
 
   return (
     <pre>
       Stripe Customer ID: {data.stripeCustomerId} <br />
       Subscriptions: {JSON.stringify(data.activePlans || [])}
     </pre>
-  );
+  )
 }
 
 function SubscribeToPlan(props) {
-  const stripe = useStripe();
-  const elements = useElements();
-  const user = useUser();
+  const stripe = useStripe()
+  const elements = useElements()
+  const user = useUser()
 
-  const [plan, setPlan] = useState();
-  const [subscriptions, setSubscriptions] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [plan, setPlan] = useState()
+  const [subscriptions, setSubscriptions] = useState([])
+  const [loading, setLoading] = useState(false)
 
   // Get current subscriptions on mount
   useEffect(() => {
-    getSubscriptions();
-  }, [user]);
+    getSubscriptions()
+  }, [user])
 
   // Fetch current subscriptions from the API
   const getSubscriptions = async () => {
     if (user) {
-      const subs = await fetchFromAPI('subscriptions', { method: 'GET' });
-      setSubscriptions(subs);
+      const subs = await fetchFromAPI('subscriptions', { method: 'GET' })
+      setSubscriptions(subs)
     }
-  };
+  }
 
   // Cancel a subscription
   const cancel = async (id) => {
-    setLoading(true);
-    await fetchFromAPI('subscriptions/' + id, { method: 'PATCH' });
-    alert('canceled!');
-    await getSubscriptions();
-    setLoading(false);
-  };
+    setLoading(true)
+    await fetchFromAPI('subscriptions/' + id, { method: 'PATCH' })
+    alert('canceled!')
+    await getSubscriptions()
+    setLoading(false)
+  }
 
   // Handle the submission of card details
   const handleSubmit = async (event) => {
-    setLoading(true);
-    event.preventDefault();
+    setLoading(true)
+    event.preventDefault()
 
-    const cardElement = elements.getElement(CardElement);
+    const cardElement = elements.getElement(CardElement)
 
     // Create Payment Method
     const { paymentMethod, error } = await stripe.createPaymentMethod({
       type: 'card',
       card: cardElement,
-    });
+    })
 
     if (error) {
-      alert(error.message);
-      setLoading(false);
-      return;
+      alert(error.message)
+      setLoading(false)
+      return
     }
 
     // Create Subscription on the Server
@@ -83,36 +82,36 @@ function SubscribeToPlan(props) {
         plan,
         payment_method: paymentMethod.id,
       },
-    });
+    })
 
     // The subscription contains an invoice
-    // If the invoice's payment succeeded then you're good, 
+    // If the invoice's payment succeeded then you're good,
     // otherwise, the payment intent must be confirmed
 
-    const { latest_invoice } = subscription;
+    const { latest_invoice } = subscription
 
     if (latest_invoice.payment_intent) {
-      const { client_secret, status } = latest_invoice.payment_intent;
+      const { client_secret, status } = latest_invoice.payment_intent
 
       if (status === 'requires_action') {
         const { error: confirmationError } = await stripe.confirmCardPayment(
           client_secret
-        );
+        )
         if (confirmationError) {
-          console.error(confirmationError);
-          alert('unable to confirm card');
-          return;
+          console.error(confirmationError)
+          alert('unable to confirm card')
+          return
         }
       }
 
       // success
-      alert('You are subscribed!');
-      getSubscriptions();
+      alert('You are subscribed!')
+      getSubscriptions()
     }
 
-    setLoading(false);
-    setPlan(null);
-  };
+    setLoading(false)
+    setPlan(null)
+  }
 
   return (
     <>
@@ -122,7 +121,7 @@ function SubscribeToPlan(props) {
         Firestore in realtime.
       </p>
       <AuthCheck fallback={<SignIn />}>
-        <div className="well">
+        <div className='well'>
           <h2>Firestore Data</h2>
           <p>User's data in Firestore.</p>
           {user?.uid && <UserData user={user} />}
@@ -130,28 +129,34 @@ function SubscribeToPlan(props) {
 
         <hr />
 
-        <div className="well">
+        <div className='well'>
           <h3>Step 1: Choose a Plan</h3>
 
           <button
             className={
               'btn ' +
-              (plan === 'plan_HC2o83JbeowZnP'
-                ? 'btn-primary'
+              (plan === 'price_1LPUZCEVBJjeleBiVT8navzF' // it's called 'price...' now?
+                ? // (plan === 'plan_HC2o83JbeowZnP'// demo
+                  'btn-primary'
                 : 'btn-outline-primary')
             }
-            onClick={() => setPlan('plan_HC2o83JbeowZnP')}>
+            onClick={() => setPlan('price_1LPUZCEVBJjeleBiVT8navzF')}
+            // onClick={() => setPlan('plan_HC2o83JbeowZnP')} // demo
+          >
             Choose Monthly $25/m
           </button>
 
           <button
             className={
               'btn ' +
-              (plan === 'plan_HD6rlaovzAiM7B'
-                ? 'btn-primary'
+              (plan === 'price_1LPUZCEVBJjeleBiJi06P219'
+                ? // (plan === 'plan_HD6rlaovzAiM7B' // demo
+                  'btn-primary'
                 : 'btn-outline-primary')
             }
-            onClick={() => setPlan('plan_HD6rlaovzAiM7B')}>
+            onClick={() => setPlan('price_1LPUZCEVBJjeleBiJi06P219')}
+            // onClick={() => setPlan('plan_HD6rlaovzAiM7B')} //demo
+          >
             Choose Quarterly $50/q
           </button>
 
@@ -161,7 +166,7 @@ function SubscribeToPlan(props) {
         </div>
         <hr />
 
-        <form onSubmit={handleSubmit} className="well" hidden={!plan}>
+        <form onSubmit={handleSubmit} className='well' hidden={!plan}>
           <h3>Step 2: Submit a Payment Method</h3>
           <p>Collect credit card details</p>
           <p>
@@ -174,12 +179,12 @@ function SubscribeToPlan(props) {
           <hr />
 
           <CardElement />
-          <button className="btn btn-success" type="submit" disabled={loading}>
+          <button className='btn btn-success' type='submit' disabled={loading}>
             Subscribe & Pay
           </button>
         </form>
 
-        <div className="well">
+        <div className='well'>
           <h3>Manage Current Subscriptions</h3>
           <div>
             {subscriptions.map((sub) => (
@@ -187,9 +192,10 @@ function SubscribeToPlan(props) {
                 {sub.id}. Next payment of {sub.plan.amount} due{' '}
                 {new Date(sub.current_period_end * 1000).toUTCString()}
                 <button
-                  className="btn btn-sm btn-danger"
+                  className='btn btn-sm btn-danger'
                   onClick={() => cancel(sub.id)}
-                  disabled={loading}>
+                  disabled={loading}
+                >
                   Cancel
                 </button>
               </div>
@@ -197,12 +203,12 @@ function SubscribeToPlan(props) {
           </div>
         </div>
 
-        <div className="well">
+        <div className='well'>
           <SignOut user={user} />
         </div>
       </AuthCheck>
     </>
-  );
+  )
 }
 
 export default function Subscriptions() {
@@ -210,5 +216,5 @@ export default function Subscriptions() {
     <Suspense fallback={'loading user'}>
       <SubscribeToPlan />
     </Suspense>
-  );
+  )
 }
