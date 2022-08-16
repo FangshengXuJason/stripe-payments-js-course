5b5940af5389940cc885c74bc69066cd45515763
FangshengXu <91645998+FangshengXuJason@users.noreply.github.com>
2022-08-15 23:37:17 -0700

    update local api prefix

--- react-app/src/helpers.js
+++ react-app/src/helpers.js
@@ -1,6 +1,8 @@
 import { auth } from './firebase'
 // const API = 'https://stripe-server-apw6lsu5yq-uc.a.run.app';
-const API = 'http://localhost:3333'
+// const API = 'http://localhost:3333'
+const API = 'http://10.131.130.98:5002/test-project-1-d10d8/us-central1'
+
 
 /**
  * A helper function to fetch data from your API.
